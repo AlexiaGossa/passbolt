58f856bd93f6bb109e34b78d5bd2121ce33e97c4
AlexiaGossa <102846702+AlexiaGossa@users.noreply.github.com>
2026-03-14 19:39:37 +0100

    Add files via upload

---
+++ certificates/prepare/prepare.cmd
@@ -0,0 +1,25 @@
+mkdir .\0_root\certs
+mkdir .\0_root\crl
+mkdir .\0_root\csr
+mkdir .\0_root\newcerts
+mkdir .\0_root\private
+
+mkdir .\1_intermediate\certs
+mkdir .\1_intermediate\crl
+mkdir .\1_intermediate\csr
+mkdir .\1_intermediate\newcerts
+mkdir .\1_intermediate\private
+
+mkdir .\2_server
+
+type nul > .\0_root\index.txt
+echo 1000 > .\0_root\serial
+
+type nul > .\1_intermediate\index.txt
+echo 2000 > .\1_intermediate\serial
+echo 3000 > .\1_intermediate\crlnumber
+
+copy /Y .\conf-root.conf .\0_root\root.conf
+copy /Y .\conf-intermediate.conf .\1_intermediate\intermediate.conf
+copy /Y .\conf-wildcard.monentreprise.fr.conf .\2_server\wildcard.monentreprise.fr.conf
+
